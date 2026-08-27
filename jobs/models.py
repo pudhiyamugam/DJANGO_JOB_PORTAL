85f1250d62e7295fdfff7820f5ee70382082c071
pudhiyamugam <jerinjuleythomas@gmail.com>
2026-08-27 16:52:05 +0530

    implemented a new table application to control the spplications

--- jobs/models.py
+++ jobs/models.py
@@ -18,3 +18,38 @@ class Job(models.Model):
 
     def __str__(self):
         return self.title
+
+class Application(models.Model):
+
+    job=models.ForeignKey(
+        Job,
+        on_delete=models.CASCADE,
+        related_name="applications"
+    )
+
+    applicant=models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="applications"
+    )
+
+    applied_at=models.DateTimeField(
+        auto_now_add=True
+    )
+
+    status=models.CharField(
+        max_length=20,
+        default="applied"
+    )
+
+    class Meta:
+
+        constraints=[
+            models.UniqueConstraint(
+                fields=["job","applicant"],
+                name="unique_job_applicant"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.applicant.username} {self.job.title}"
